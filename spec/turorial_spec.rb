a2fc906b672587144f6f4362b9cc4e5676e3b3fa
Pceja <perla.c.c.c@gmail.com>
2021-12-27 16:28:30 -0600

    Add first tests

--- spec/turorial_spec.rb
+++ spec/turorial_spec.rb
@@ -1,3 +1,12 @@
-RSpec.describe Turorial do
+require "watir"
+require "webdrivers"
 
+RSpec.describe Turorial do
+    it "should explore California" do
+        browser = Watir::Browser.new
+        browser.goto "https://explorecalifornia.org/"
+        
+        expect(browser.text_field(id: "name").exists?).to equal(true)
+        expect(browser.textarea(id: "comments").exists?).to equal(true)
+    end
 end
