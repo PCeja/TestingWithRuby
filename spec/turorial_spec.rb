d34b411f2bdc522c833a11177ce760ee6ed37055
Perla <perla.c.c.c@gmail.com>
2021-12-28 17:16:39 -0600

    Feature/ruby test with watir (#1)
    * Add Ruby image in README
    
    * Add & to README
    
    * Add webdrivers dependency
    
    * Remove out of the box test
    
    * Add first tests
    
    * Use MIT license
    
    * Update gitignore
    
    * Update  bundler version
    
    * Add CODE_OF_CONDUCT.md, Gemfile.lock and git ignore for intellij
    
    * Update test URL'should explore California'
    
    * Replace 'comments' element to test a radio
    
    * Add assertions for set values in elements
    
    Co-authored-by: pceja <pceja@Perlas-MacBook-Air.local>

--- spec/turorial_spec.rb
+++ spec/turorial_spec.rb
@@ -1,9 +1,19 @@
-RSpec.describe Turorial do
-  it "has a version number" do
-    expect(Turorial::VERSION).not_to be nil
-  end
+require "watir"
+require "webdrivers"
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+RSpec.describe Turorial do
+    it "should explore California" do
+        browser = Watir::Browser.new
+        browser.goto "https://explorecalifornia.org/support.htm"
+        
+        expect(browser.text_field(id: "name").exists?).to equal(true)
+        expect(browser.radio(id: "tourStatus_0").exists?).to equal(true)
+
+        browser.text_field(id: "name").set "Perla Ceja"
+        browser.radio(id: "tourStatus_0").set
+
+        expect(browser.text_field(id: "name").value).to match("Perla Ceja")
+        expect(browser.radio(id: "tourStatus_0").set?).to equal(true)
+
+    end
 end
--- turorial.gemspec
+++ turorial.gemspec
@@ -36,8 +36,9 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_development_dependency "bundler", "~> 1.17"
+  spec.add_development_dependency "bundler", "2.2.32"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
   spec.add_dependency "watir", "~> 6.16"
+  spec.add_dependency "webdrivers", "~> 4.0"
 end
