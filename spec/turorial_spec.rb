40c6848a7eed296dfa9efebd010b0cb285321ebd
Pceja <perla.c.c.c@gmail.com>
2021-12-28 17:06:50 -0600

    Replace 'comments' element to test a radio

--- spec/turorial_spec.rb
+++ spec/turorial_spec.rb
@@ -7,6 +7,6 @@ RSpec.describe Turorial do
         browser.goto "https://explorecalifornia.org/support.htm"
         
         expect(browser.text_field(id: "name").exists?).to equal(true)
-        expect(browser.textarea(id: "comments").exists?).to equal(true)
+        expect(browser.radio(id: "tourStatus_0").exists?).to equal(true)
     end
 end
