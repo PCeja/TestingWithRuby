e2bd706b0a925e14152c1a43bbc0a5e524d1c651
Pceja <perla.c.c.c@gmail.com>
2021-12-28 17:08:10 -0600

    Add assertions for set values in elements

--- spec/turorial_spec.rb
+++ spec/turorial_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe Turorial do
         
         expect(browser.text_field(id: "name").exists?).to equal(true)
         expect(browser.radio(id: "tourStatus_0").exists?).to equal(true)
+
+        browser.text_field(id: "name").set "Perla Ceja"
+        browser.radio(id: "tourStatus_0").set
+
+        expect(browser.text_field(id: "name").value).to match("Perla Ceja")
+        expect(browser.radio(id: "tourStatus_0").set?).to equal(true)
+
     end
 end
